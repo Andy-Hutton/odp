fa481d9d7e3ee9bb9521a5e6cd164f305d97e9d8
Andy Hutton <shandy_hutton@hotmail.com>
2023-02-09 11:25:39 +0000

    clicks

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let totalClicks = 0;
+let maxClicks = 25;
+
 function Product(name, src) {
   this.name = name;
   this.src = src;
@@ -8,6 +11,12 @@ function Product(name, src) {
 
 Product.allProducts = [];
 
+// using this array, create a new product for each item
+// the name of the product should be the item in the array
+// the src of the product should be like so:
+// images/PRODUCTNAME.jpg
+
+// array is in discord chat:
 const productNames = [
   "bag",
   "banana",
@@ -31,24 +40,36 @@ const productNames = [
 for (let i = 0; i < productNames.length; i++) {
   new Product(productNames[i], `images/${productNames[i]}.jpeg`);
 }
+
+// use Google to help you
+// wrtie a function that returns a random number
+// the number will represent an index value for one of the items in the Product.allProducts array
+
 function randomProductIndex() {
   return Math.floor(Math.random() * Product.allProducts.length);
 }
 
-function renderimages() {
+// write a function to render our images
+// have the images be chosen randomly from our Product.allProducts array
+// hint, use randomProductIndex() and bracket notation to access the item in the array
+function renderImages() {
+  // get three random indexes for my product array
   let index1 = randomProductIndex();
   let index2 = randomProductIndex();
   let index3 = randomProductIndex();
 
+  // make sure none of them are the same
   while (index1 === index2 || index1 === index3 || index2 === index3) {
     index2 = randomProductIndex();
     index3 = randomProductIndex();
   }
 
+  // retrieve our image elements
   let img1 = document.getElementById("img1");
   let img2 = document.getElementById("img2");
   let img3 = document.getElementById("img3");
 
+  // change the src attribute of img1, img2 & img3 to be the src from our random products
   img1.src = Product.allProducts[index1].src;
   img2.src = Product.allProducts[index2].src;
   img3.src = Product.allProducts[index3].src;
@@ -57,27 +78,39 @@ function renderimages() {
   img2.alt = Product.allProducts[index2].name;
   img3.alt = Product.allProducts[index3].name;
 
-  // Product.allProducts[index1].views++;
-  // Product.allProducts[index2].views++;
-  // Product.allProducts[index3].views++;
+  Product.allProducts[index1].views++;
+  Product.allProducts[index2].views++;
+  Product.allProducts[index3].views++;
 }
 
+renderImages();
+
+// increase the clicks on the clicked Product object (for loop and clicks++)(check the event.target.alt)
+// make sure the user is clicking on one of the images
 function handleClick(event) {
+  // check if the thing we clicked on is the container (as aposed to an image)
   if (event.target === imgContainer) {
-    alert("Please click an the image.");
-    return;
+    alert("Please click one of the images, not inbetween the images.");
+    return; // this return stops the function
   }
 
+  // check every single products "name" against the alt tag of the target, and increase the clicks
   for (let i = 0; i < Product.allProducts.length; i++) {
     if (event.target.alt === Product.allProducts[i].name) {
       Product.allProducts[i].clicks++;
-      break;
+      break; // stop the loop, because we found our product
     }
   }
-  renderimages();
+  totalClicks++;
+  if (totalClicks === maxClicks) {
+    alert("thanks for voting");
+    imgContainer.removeEventListener("click", handleClick);
+    return;
+  }
+
+  // get three new images
+  renderImages();
 }
 
 const imgContainer = document.getElementById("img-container");
 imgContainer.addEventListener("click", handleClick);
-
-renderimages();
